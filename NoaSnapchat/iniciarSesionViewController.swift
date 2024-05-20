7d50b9ebb60b497d9ccf6a195a8b5dc3edba937b
RampageWallE <pier_201612@hotmail.com>
2024-05-20 15:29:37 -0500

    aasda

--- NoaSnapchat/iniciarSesionViewController.swift
+++ NoaSnapchat/iniciarSesionViewController.swift
@@ -6,8 +6,30 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseCore
+import GoogleSignIn
 
-class ViewController: UIViewController {
+class iniciarSesionViewController: UIViewController {
+    
+    //VARIABLES
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    //ACTIONS
+    @IBAction func iniciarSesionTapped(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!){(user, error) in
+            print("Intentando iniciar Sesion")
+            if error != nil {
+                print("Parece que tuviste algun error: \(error)")
+            }else{
+                print("Inicio de sesion exitoso")
+            }
+        }
+    }
+    
+    //FUNCTIONS
 
     override func viewDidLoad() {
         super.viewDidLoad()
