a7958ddd8cf087bc0668ea0b64b11ad248086ba2
Piero Valentino Noa Chahuayo <pierovalentinonoachahuayo@MacBook-Air-de-Piero.local>
2024-06-09 12:11:00 -0500

    LAB 13 CASI FINALIZADO

--- NoaSnapchat/ViewControllers/SnapsViewController.swift
+++ NoaSnapchat/ViewControllers/SnapsViewController.swift
@@ -40,12 +40,37 @@ class SnapsViewController: UIViewController, UITableViewDelegate, UITableViewDat
             snap.imagenURL = (snapshot.value as! NSDictionary)["imagenURL"] as! String
             snap.from = (snapshot.value as! NSDictionary)["from"] as! String
             snap.descrip = (snapshot.value as! NSDictionary)["description"] as! String
+            snap.id = snapshot.key
             self.snaps.append(snap)
             self.tablaSnaps.reloadData()
         })
+        Database.database().reference().child("usuarios").child((Auth.auth().currentUser?.uid)!).child("snaps").observe(DataEventType.childRemoved, with: { (snapshot) in
+            var iterator = 0
+            for snap in self.snaps{
+                if snap.id == snapshot.key{
+                    self.snaps.remove(at: iterator)
+                }
+                iterator+=1
+            }
+            self.tablaSnaps.reloadData()
+        })
+        
     }
     
-
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let snap = snaps[indexPath.row]
+        performSegue(withIdentifier: "versnapsegue", sender: snap)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "versnapsegue" {
+            let siguienteVC = segue.destination as! VerSnapViewController
+            siguienteVC.snap = sender as! Snap
+        }
+    }
+    
+    
+    
     /*
     // MARK: - Navigation
 
--- NoaSnapchat/ViewControllers/VerSnapViewController.swift
+++ NoaSnapchat/ViewControllers/VerSnapViewController.swift
@@ -6,15 +6,28 @@
 //
 
 import UIKit
+//import SDWebImage
+import Firebase
 
 class VerSnapViewController: UIViewController {
 
+
+    @IBOutlet weak var lblMensaje: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var snap = Snap()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        lblMensaje.text =  "Mensaje: " + snap.descrip
+        //imageView.sd_setImage(with: URL(string: snap.imagenURL), completed: nil)
+        
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        Database.database().reference().child("usuarios").child((Auth.auth().currentUser?.uid)!).child("snaps").child(snap.id).removeValue()
+    }
 
     /*
     // MARK: - Navigation
